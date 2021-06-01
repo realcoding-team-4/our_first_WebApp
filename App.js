95407b28b68c6873a1f7a9ddba67b77b0ccf3389
Kim Seong Han <k0s0a7@gmail.com>
2021-06-01 23:40:28 +0900

    1 ~ 14 process with team 4

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Button, StyleSheet, Text, View } from 'react-native';
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>1</Text>
+                <Text style={styles.text}>2</Text>
+                <Text style={styles.text}>3</Text>
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    text: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'red',
+    },
 });
