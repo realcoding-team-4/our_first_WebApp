8c1c30596022046af78e2330b550dd457fafd9f1
Kim Seong Han <k0s0a7@gmail.com>
2021-06-04 01:24:35 +0900

    38 process with team 4

---
+++ WeatherDetailScreen.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Constants from 'expo-constants';
+
+const {
+  apiKey,
+  baseUrl,
+  region,
+} = Constants.manifest.extra.openWeatherApi;
+
+const queryUrl = (city) => `${baseUrl}/weather?q=${city}&appid=${apiKey}&lang=${region}`
+
+export default class WeatherDetailScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    const {
+      route: {
+        params: { city },
+      },
+    } = this.props;
+
+    fetch(queryUrl(city))
+      .then(response => response.json())
+      .then(info => {
+        console.log(info);
+        this.setState({
+          ...info,
+          isLoading: false,
+        });
+      });
+  }
+
+  render() {
+    const {
+      route: {
+        params: { city },
+      },
+      navigation,
+    } = this.props;
+
+    navigation.setOptions({ title: `Weather Information: ${city}` });
+
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <Text>데이터를 불러오는 중입니다.</Text>
+        </View>
+      )
+    }
+
+    let celsius = this.state.main.temp - 273.15;
+
+    return (
+      <View style={styles.container}>
+        <Text>온도: {celsius.toFixed(1)}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
